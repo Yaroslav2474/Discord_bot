dc9082b2a210d9e1a0ed7ef8fa697244f2cc68e1
Yaroslav2474 <aroslavserbakov93510@gmail.com>
2026-05-23 23:48:06 +0300

    update file v1.1

--- main.go
+++ main.go
@@ -13,20 +13,18 @@ import (
 
 type Command struct {
 	Keywords []string `json:"keywords"`
-	Replay   string   `json:"replay"`
+	Replay   string   `json:"replay,omitempty"`
+	Reaction string   `json:"reaction,omitempty"`
 }
 
 var Rules []Command
-var TokenBot = "YOUR BOT TOKEN"
 
 func loadCommands(filename string) error {
 	bytes, err := os.ReadFile(filename)
 	if err != nil {
 		return err
 	}
-
 	return json.Unmarshal(bytes, &Rules)
-
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -36,10 +34,58 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	userMessage := strings.ToLower(m.Content)
 
+	if strings.Contains(userMessage, "хуй") {
+		emoji := []string{"%F0%9F%87%AD", "%f0%9f%87%ba", "%F0%9F%87%AE"}
+		for _, e := range emoji {
+			err := s.MessageReactionAdd(m.ChannelID, m.ID, e)
+			if err != nil {
+				fmt.Println("Error adding letter reaction: ", err)
+			}
+		}
+		return
+	}
+
+	if strings.Contains(userMessage, "матвей") {
+		imagePath := "matvey.png"
+
+		file, err := os.Open(imagePath)
+		if err != nil {
+			fmt.Println("Error opening image for Matvey: ", err)
+			s.ChannelMessageSend(m.ChannelID, "Не удалось найти картинку для Матвея!")
+			return
+		}
+		defer file.Close()
+
+		msgData := &discordgo.MessageSend{
+			Content: "На месте епт",
+			Files: []*discordgo.File{
+				{
+					Name:        "matvey.png",
+					ContentType: "image/png",
+					Reader:      file,
+				},
+			},
+		}
+
+		_, err = s.ChannelMessageSendComplex(m.ChannelID, msgData)
+		if err != nil {
+			fmt.Println("Error sending image for Matvey: ", err)
+		}
+		return
+	}
+
 	for _, rule := range Rules {
 		for _, keyword := range rule.Keywords {
-			if userMessage == keyword {
-				s.ChannelMessageSend(m.ChannelID, rule.Replay)
+			if strings.Contains(userMessage, strings.ToLower(keyword)) {
+				if rule.Replay != "" {
+					s.ChannelMessageSend(m.ChannelID, rule.Replay)
+				}
+				if rule.Reaction != "" {
+					err := s.MessageReactionAdd(m.ChannelID, m.ID, rule.Reaction)
+					if err != nil {
+						fmt.Println("Error adding reaction: ", err)
+					}
+				}
 				return
 			}
 		}
@@ -47,9 +93,12 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 }
 
 func main() {
+	TokenBot := os.Getenv("DISCORD_TOKEN")
+	if TokenBot == "" {
+		panic("Error: bot token not initialization")
+	}
 
 	err := loadCommands("command.json")
-
 	if err != nil {
 		fmt.Println("Error load json: ", err)
 		return
@@ -57,16 +106,15 @@ func main() {
 	fmt.Println("json loaded successfully")
 
 	bt, err := discordgo.New("Bot " + TokenBot)
-
 	if err != nil {
 		fmt.Println("Error creating discord session: ", err)
 		return
 	}
 
+	bt.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildMessageReactions
 	bt.AddHandler(messageCreate)
 
 	err = bt.Open()
-
 	if err != nil {
 		fmt.Println("Error opening connection: ", err)
 		return
@@ -76,9 +124,7 @@ func main() {
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-
 	<-sc
 
 	bt.Close()
-
 }
